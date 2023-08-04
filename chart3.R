4ded63673d6ac65608e9c2803b9087d7c684afa8
Drew King <andrew.phillip.king@gmail.com>
2023-08-04 13:25:47 -0700

    Optimized chart3 to run faster.

--- chart3.R
+++ chart3.R
@@ -4,12 +4,23 @@ library(lubridate)
 library(plotly)
 
 # Calculate city temperature change
-city_temp_change_summary <- city_annual_summary()
+city_temp_change_summary <- city_temp_change()
 
-# Function to calculate city temperature differences
+# Function to filter city temperature by change
 city_temp_differences <- function(temp_summary) {
   temp_diff_data <- temp_summary %>%
-    filter(event_type == "chg_temp")
+    convert_coords() %>%
+    reframe(
+      Country,
+      City,
+      dt,
+      TempChange = AverageTemperature,
+      TempChangeMax = MaxAverageTemperature,
+      TempChangeMin = MinAverageTemperature,
+      Uncertainty = AverageTemperatureUncertainty,
+      lat,
+      lng
+    )
   return(temp_diff_data)
 }
 
@@ -17,15 +28,20 @@ city_temp_differences <- function(temp_summary) {
 city_temp_diff_data <- city_temp_differences(city_temp_change_summary)
 
 # Create the Bubble chart
-city_plot <- ggplot(city_temp_diff_data, aes(x = lng, y = lat,
-                                size = AverageTemperatureUncertainty)) +
-  geom_point(alpha = 0.5, aes(colour = AverageTemperature)) +
+city_plot <- ggplot(city_temp_diff_data) +
+  geom_point(alpha = 0.5, mapping = aes(
+    x = lng, y = lat,
+    colour = TempChange,
+    size = Uncertainty,
+    text = paste0("Country: ", Country, "<br>",
+                 "City: ", City)
+    )) +
   scale_size_continuous(range = c(1, 5)) +  # Adjust the range of bubble sizes
   labs(title = "City Temperature Changes",
-       subtitle = "Change in city temperatures from 1850-2013",
+       subtitle = "Change in city temperatures from 1850-2013", # TODO: Not viz
        x = "Longitude",
        y = "Latitude",
-       size = "Temperature Uncertainty",
+       size = "Temperature Uncertainty", # TODO: This is not showing up on chart
        color = "Temperature Difference") +
   theme_minimal()
 
