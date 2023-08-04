3cbceab6b16c0e4ac6eeb622310599a3d1075f64
Deleted-0970 <derekliuj@gmail.com>
2023-08-04 15:32:39 -0700

    adds world map underlay to chart3

--- chart3.R
+++ chart3.R
@@ -25,16 +25,26 @@ city_temp_differences <- function(temp_summary) {
 # Calculate city temperature differences using the new function
 city_temp_diff_data <- city_temp_differences(city_temp_change_summary)
 
+# world map from ggplot2
+world_map <- map_data('world')
+
 # Create the Bubble chart
-city_plot <- ggplot(city_temp_diff_data) +
-  geom_point(alpha = 0.5, mapping = aes(
+city_plot <- ggplot() +
+  geom_polygon(data = world_map, 
+               aes(
+                 x = long, 
+                 y = lat, 
+                 group = group
+               ), 
+               fill = "lightgray") +
+  geom_point(data = city_temp_diff_data, alpha = 0.5, mapping = aes(
     x = lng, y = lat,
     colour = TempChange,
     size = Uncertainty,
     text = paste0("Country: ", Country, "<br>",
                  "City: ", City)
     )) +
-  scale_size_continuous(range = c(1, 5)) +  # Adjust the range of bubble sizes
+  scale_size_continuous(range = c(0.1, 1)) +  # Adjust the range of bubble sizes
   labs(title = "City Temperature Changes",
        subtitle = "Change in city temperatures from 1850-2013", # TODO: Not viz
        x = "Longitude",
@@ -44,3 +54,4 @@ city_plot <- ggplot(city_temp_diff_data) +
   theme_minimal()
 
 ggplotly(city_plot)
+
