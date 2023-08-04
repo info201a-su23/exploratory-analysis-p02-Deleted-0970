6af403b3dfa9ef300a882bcd160311bfb203d203
Drew King <andrew.phillip.king@gmail.com>
2023-08-04 13:02:54 -0700

    Knitted and made chart3 a plotly plot.

--- chart3.R
+++ chart3.R
@@ -1,6 +1,7 @@
 source("analysis.R")
 library(tidyverse)
 library(lubridate)
+library(plotly)
 
 # Calculate city temperature change
 city_temp_change_summary <- city_annual_summary()
@@ -16,7 +17,7 @@ city_temp_differences <- function(temp_summary) {
 city_temp_diff_data <- city_temp_differences(city_temp_change_summary)
 
 # Create the Bubble chart
-ggplot(city_temp_diff_data, aes(x = lng, y = lat,
+city_plot <- ggplot(city_temp_diff_data, aes(x = lng, y = lat,
                                 size = AverageTemperatureUncertainty)) +
   geom_point(alpha = 0.5, aes(colour = AverageTemperature)) +
   scale_size_continuous(range = c(1, 5)) +  # Adjust the range of bubble sizes
@@ -28,3 +29,4 @@ ggplot(city_temp_diff_data, aes(x = lng, y = lat,
        color = "Temperature Difference") +
   theme_minimal()
 
+ggplotly(city_plot)
