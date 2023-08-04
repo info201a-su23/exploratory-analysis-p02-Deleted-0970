bf90933584917c7f6b66b3d3201cf96ac68e1a03
Deleted-0970 <derekliuj@gmail.com>
2023-08-04 16:10:33 -0700

    creates bar chart for third chart. Adds it to Rmd

--- chart2.R
+++ chart2.R
@@ -1,47 +1,52 @@
 source("analysis.R") # access analysis.R methods and data
 library(tidyverse)
-library(plotly) # for plotting
+# library(plotly) # for plotting
 
 # Organize Year data
-global_temp <- annual_global_temp %>% 
-  mutate(dt = as.numeric(dt)) %>%
-  arrange(dt) %>%
-  rename(Year = dt, AvgTemp = LandAverageTemperature)
+# global_temp <- annual_global_temp %>% 
+#   mutate(dt = as.numeric(dt)) %>%
+#   arrange(dt) %>%
+#   rename(Year = dt, AvgTemp = LandAverageTemperature)
 
-# Create Scatter plot
-temp_plot <- ggplot(data = global_temp, aes(x = Year, y = AvgTemp)) +
-  geom_point(size = 0.75) + 
-  labs(title = "Average Global Temperature Over Time",
-       x = "Year",
-       y = "Average Temperature"
-       ) +
-  geom_smooth(method = "loess", formula = y ~ x) +
+# Create Scatter plot of average global temp over time
+# temp_plot <- ggplot(data = global_temp, aes(x = Year, y = AvgTemp)) +
+#   geom_point(size = 0.75) + 
+#   labs(title = "Average Global Temperature Over Time",
+#        x = "Year",
+#       y = "Average Temperature"
+#        ) +
+#   geom_smooth(method = "loess", formula = y ~ x) +
+#   theme_light()
+
+selected_continents <- c(
+  "Europe", 
+  "Africa", 
+  "Asia", 
+  "Antarctica", 
+  "Oceania", 
+  "North America", 
+  "South America"
+  )
+
+continent_temp <- annual_country_temp %>%
+  filter(Country %in% selected_continents) %>%
+  filter(dt == max(dt))
+
+temp_bar <- ggplot(
+  data = continent_temp, 
+  aes(x = Country, y = AverageTemperature, fill = AverageTemperature)
+  ) +
+  geom_bar(stat = "identity") +  
+  #stat = "identity" to plot the actual values rather than count
+  scale_fill_gradient(low = "blue", high = "red") +
+  labs(title = "Average Temperature by Continent (C)",
+       x = "Continent",
+       y = "Average Temperature",
+       fill = "Average Temperature") +
   theme_light()
 
-# Calculate the first derivative (velocity of temperature change)
-# vel_global_temp <- annual_global_temp %>%
-#  mutate(dt = as.numeric(dt)) %>%
-#  arrange(dt) %>%
-#  rename(Year = dt) %>%
-#  mutate(
-#    velocity = (
-#      LandAverageTemperature - lag(LandAverageTemperature)) / (Year - lag(Year))
-#    )
+ggplotly(temp_bar)
 
-# Create the scatterplot
-# vel_plot <- ggplot(data = vel_global_temp, aes(x = Year, y = velocity)) +
-#   geom_point() +
-#   labs(title = "Velocity of Temperature Change Over Time",
-#       x = "Year",
-#       y = "Velocity"
-#       ) +
-#  geom_smooth(method = "loess", formula = y ~ x) +
-#  scale_y_log10() + theme_light()
-
-# plotly to make it interative :D
-#ggplotly(vel_plot)
-
-ggplotly(temp_plot)
-
+  
 
 
