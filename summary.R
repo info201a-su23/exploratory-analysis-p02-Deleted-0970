f24a8f3019dc7d6c3798f713c851a0ca940d661b
Deleted-0970 <derekliuj@gmail.com>
2023-08-04 17:34:25 -0700

    fixes knitting issues. All visualizations functional

--- summary.R
+++ summary.R
@@ -6,7 +6,6 @@ source("analysis.R")
 global_summary_table <- global_annual_summary()
 country_summary_table <- country_annual_summary()
 # city_summary_table <- city_annual_summary()
-# this is producing error messages.
 
 
 # Calculating values for summary
